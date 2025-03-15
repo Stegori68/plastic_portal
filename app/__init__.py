798da499300cff4e592723561260798a8b59f62a
Stegori68 <stegori@tiscali.it>
2025-03-15 19:45:29 +0100

    new file:   app/__init__.py

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+login_manager = LoginManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login_manager.init_app(app)
+    
+    from app.routes.auth import auth_bp
+    from app.routes.main import main_bp
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(main_bp)
+    
+    return app
