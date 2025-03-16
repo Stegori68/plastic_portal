ed1b0d624e27a7ae3e7cc21fcdc610129b2345b0
Stegori68 <stegori@tiscali.it>
2025-03-16 18:58:53 +0100

    modified:   app/commands.py

--- app/commands.py
+++ app/commands.py
@@ -4,6 +4,7 @@ from flask import current_app, cli
 from flask_migrate import Migrate
 from app import create_app, db
 from datetime import datetime
+from models import User
 
 app = create_app()
 migrate = Migrate(app, db)
