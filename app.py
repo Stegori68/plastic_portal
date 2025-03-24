46285140d7a26da1efc526bb152579d6ecca1d8f
Stegori68 <stegori@tiscali.it>
2025-03-24 08:37:14 +0100

    modified:   app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from plastic_portal import config
+from .config import Config
 
 app = Flask(__name__)
-app.config.from_object(config)
+app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
