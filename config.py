3671f3358ed2c4e62442287260f999e5e4b396ee
Stegori68 <stegori@tiscali.it>
2025-03-24 08:30:11 +0100

    modified:   config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+mysqlconnector://username:password@your_rds_endpoint/plastic_portal_db'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'ooysdvf9879ffkkfifihif1441'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://admin:%Onafets01!@plastic-portal-db.czw0aqcke793.eu-north-1.rds.amazonaws.com:3306/plastic_portal_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
