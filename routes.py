45d789e7ac5c1745a362a721b3344df758badd3c
Stegori68 <stegori@tiscali.it>
2025-03-25 11:01:53 +0100

    modified:   routes.py

--- routes.py
+++ routes.py
@@ -8,7 +8,8 @@ from . import app
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # return render_template('index.html')
+    return "Allora funziona!"
 
 @app.route('/login', methods=['GET, POST'])
 def login():
