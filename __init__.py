f1e891b29d6d45ac5b62642ecdd77a4ecce9b94e
Stegori68 <stegori@tiscali.it>
2025-03-25 11:27:35 +0100

    modified:   __init__.py

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from .config import Config
+from .models import User
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,5 +12,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 from . import routes, models
