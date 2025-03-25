4a7fc4292e917ebee90a925a1a8dd2397b518feb
Stegori68 <stegori@tiscali.it>
2025-03-25 18:49:40 +0100

    modified:   models.py

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from flask_login import UserMixin
 from datetime import datetime
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
