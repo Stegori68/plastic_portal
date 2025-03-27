7570eccab8c1b75a537fda0dd58f62e7a9f8b6db
Stegori68 <stegori@tiscali.it>
2025-03-27 13:17:08 +0100

    renamed:    OLD_app.py -> app.py 	modified:   templates/admin/material_management.html

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 if __name__ == '__main__':
     app.run(debug=True)
