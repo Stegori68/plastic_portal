e85bb632b80be771ccbe8da4a64080137287cb91
Stegori68 <stegori@tiscali.it>
2025-03-27 17:17:23 +0100

    modified:   routes.py

--- routes.py
+++ routes.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, flash, request
 from plastic_portal import app, db
 from plastic_portal.forms import LoginForm, QuoteForm, RegistrationForm, MaterialForm, ProductionForm, UserForm
-from plastic_portal.models import User, Material, Production, Quote
+from plastic_portal.models import User, Material, Production, Quote, ProductCategory, ProductBrand
 from flask_login import login_user, logout_user, current_user, login_required
 from datetime import date
 from . import app
